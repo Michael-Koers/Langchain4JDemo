4b49d576b31e5e6b6542fd10ed6f16da76532be9
Michael <michaelkoers95@gmail.com>
2024-07-30 22:26:52 +0200

    Work on EasyRAG

---
+++ src/main/java/com/michael/koers/nljug/EasyRAG.java
@@ -0,0 +1,44 @@
+package com.michael.koers.nljug;
+
+import com.michael.koers.examples.ApiKeys;
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+
+import java.nio.file.Path;
+
+
+public class EasyRAG {
+
+    public static void main(String[] args) {
+
+        var embeddingStore = loadDocument(Path.of("C:\\Users\\Michael\\Documents\\langchain\\Martin-and-Donny.txt"));
+
+        RAGAssistant assistant = AiServices.builder(RAGAssistant.class)
+                .chatLanguageModel(OpenAiChatModel.withApiKey(ApiKeys.OPENAI_PAID))
+                .contentRetriever(EmbeddingStoreContentRetriever.from(embeddingStore))
+                .build();
+
+        String answer = assistant.chat("Where are Martin and Donny?");
+        System.out.println(answer);
+
+    }
+
+    private static InMemoryEmbeddingStore<TextSegment> loadDocument(Path path) {
+        Document document = FileSystemDocumentLoader.loadDocument(path);
+        InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        EmbeddingStoreIngestor.ingest(document, embeddingStore);
+        return embeddingStore;
+    }
+}
+
+
+interface RAGAssistant {
+    String chat(String message);
+}
